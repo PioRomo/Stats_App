3bb9bf4139be7802eec7b38084c814e187bd5803
PioRomo <promocolleges@gmail.com>
2023-04-08 11:00:27 -0700

    Created basics of the app:  Modified background color and text color  Created Calculate Button and AtBat & Hits Text Fields  Created Calculate function that returns a textview

--- app/src/main/java/com/example/stats_app/MainActivity.java
+++ app/src/main/java/com/example/stats_app/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.stats_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
-
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
+    TextView output = findViewById(R.id.output);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void calculate(View v){
+        TextView atBats = findViewById(R.id.inputAtBats);
+        TextView hits = findViewById(R.id.inputHits);
+        int input_atBats = Integer.parseInt(atBats.getText().toString());
+        int input_hits = Integer.parseInt(hits.getText().toString());
+        int battingAverage = input_hits / input_atBats;
+        output.setText("BATTING AVERAGE: " + battingAverage);
+
+
+
+
     }
 }

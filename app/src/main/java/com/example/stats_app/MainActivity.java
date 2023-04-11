b084e4a560557fe7aa55d4e0c1b3cc144849dcad
PioRomo <promocolleges@gmail.com>
2023-04-11 12:14:49 -0700

    Added OBP function to MainActivity.java

--- app/src/main/java/com/example/stats_app/MainActivity.java
+++ app/src/main/java/com/example/stats_app/MainActivity.java
@@ -14,11 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void calculate(View v){
-        battingAverage_calc(v);
+        TextView output = findViewById(R.id.output);
+        battingAverage_calc(v, output);
+        onBasePercentage_calc(v, output);
     }
 
-    public void battingAverage_calc(View v){
-        TextView output = findViewById(R.id.output);
+    public void battingAverage_calc(View v, TextView output){
         TextView atBats = findViewById(R.id.inputAtBats);
         TextView hits = findViewById(R.id.inputHits);
         //fetch inputs and calculate the batting average
@@ -28,11 +29,30 @@ public class MainActivity extends AppCompatActivity {
             double input_hits = Integer.parseInt(hits.getText().toString());
             double battingAverage = input_hits / input_atBats;
             DecimalFormat df = new DecimalFormat("#.###");
-            output.setText("BATTING AVERAGE: " + df.format(battingAverage));
+            output.setText("BATTING AVERAGE: " + df.format(battingAverage) + "\n");
         }catch(NumberFormatException nfe){
-            output.setText("Invalid input. Try again.");
+            output.setText("Error calculating BA. Make sure values are valid." + "\n");
         }
 
 
+    }
+
+    public void onBasePercentage_calc(View v, TextView output){
+        TextView atBats = findViewById(R.id.inputAtBats);
+        TextView hits = findViewById(R.id.inputHits);
+        TextView walks = findViewById(R.id.Walks);
+        TextView hbp = findViewById(R.id.HBP);
+
+        try{
+            double input_atBats = Integer.parseInt(atBats.getText().toString());
+            double input_hits = Integer.parseInt(hits.getText().toString());
+            double input_walks = Integer.parseInt(walks.getText().toString());
+            double input_hbp = Integer.parseInt(walks.getText().toString());
+            double onBasePercentage = (input_hits + input_walks + input_hbp) / (input_atBats + input_walks + input_hbp);
+            DecimalFormat df = new DecimalFormat("#.###");
+            output.append("OBP: " + df.format(onBasePercentage) + "\n");
+        }catch(NumberFormatException nfe){
+            output.setText("Error calculating OBP. Make sure values are valid." + "\n");
+        }
     }
 }

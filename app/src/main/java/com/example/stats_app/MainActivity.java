9c59759eb20c59d8f0cdf9935d9000d761d35696
PioRomo <promocolleges@gmail.com>
2023-04-11 09:54:55 -0700

    Fixed app crash issue by removing var declaration at top of MainActivity.java Modified MainActivity.java - Created BattingAverage helper function and added into calculateButtons functionality

--- app/src/main/java/com/example/stats_app/MainActivity.java
+++ app/src/main/java/com/example/stats_app/MainActivity.java
@@ -4,23 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.os.Bundle;
 import android.widget.TextView;
+import java.text.DecimalFormat;
 public class MainActivity extends AppCompatActivity {
 
-    TextView output = findViewById(R.id.output);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void calculate(View v){
+        battingAverage_calc(v);
+    }
+
+    public void battingAverage_calc(View v){
+        TextView output = findViewById(R.id.output);
         TextView atBats = findViewById(R.id.inputAtBats);
         TextView hits = findViewById(R.id.inputHits);
-        int input_atBats = Integer.parseInt(atBats.getText().toString());
-        int input_hits = Integer.parseInt(hits.getText().toString());
-        int battingAverage = input_hits / input_atBats;
-        output.setText("BATTING AVERAGE: " + battingAverage);
+        //fetch inputs and calculate the batting average
 
-
+        try{
+            double input_atBats = Integer.parseInt(atBats.getText().toString());
+            double input_hits = Integer.parseInt(hits.getText().toString());
+            double battingAverage = input_hits / input_atBats;
+            DecimalFormat df = new DecimalFormat("#.###");
+            output.setText("BATTING AVERAGE: " + df.format(battingAverage));
+        }catch(NumberFormatException nfe){
+            output.setText("Invalid input. Try again.");
+        }
 
 
     }
